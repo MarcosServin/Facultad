af7c3f05fd5f508156bc5d6b1a9f2a2836ff58f3
MS:. <50627502+MarcosServin@users.noreply.github.com>
2025-08-29 19:57:05 -0300

    Update PRACTICA1.pas

--- Taller/PRACTICA1.pas
+++ Taller/PRACTICA1.pas
@@ -45,3 +45,98 @@
 //     cargarector(a,dimL);
 //     insercion(a,dimL);
 // end.
+
+program uno;
+const
+    dimF = 300;
+type
+    rango = 1..dimF;
+    oficina = record
+        codigo:integer;
+        dni:LongInt;
+        valor:integer;
+    end;
+    vector = array [rango] of oficina;
+//
+procedure LeerOficina(var off:oficina);
+begin
+    WriteLn('Codigo: '); ReadLn(off.codigo);
+    if (off.codigo <> -1) then
+    begin
+        WriteLn('Dni: '); ReadLn(off.dni);
+        WriteLn('Valor: '); ReadLn(off.valor);
+    end;
+end;
+//
+procedure CargarOficinas(var v:vector;var dimL:integer);
+var
+    off:oficina;
+begin
+    LeerOficina(off);
+    while (off.codigo <> -1) and (dimL<dimF) do
+    begin
+        dimL:=dimL+1;
+        v[dimL]:=off;
+        LeerOficina(off);
+    end;
+end;
+//
+Procedure seleccion ( var v: vector; dimL: Integer );
+var 
+    i, j, pos: Integer; 
+    actual:oficina;
+begin
+    for i := 1 to diml-1 do begin 
+      pos:= i;
+      for j:= i+1 to diml do 
+        if (v[j].codigo < v[pos].codigo)then 
+            pos:=j ; 
+        actual:= v[pos]; 
+        v[pos]:= v[i];  
+        v[i]:= actual;    
+    end;
+  end;
+
+
+procedure ImprimirVector(v:vector;dimL:integer);
+var
+    i:integer;
+begin
+    for i:=1 to dimL do
+    begin
+        WriteLn('Codigo: ',v[i].codigo);
+        WriteLn('Dni: ',v[i].dni);
+        WriteLn('Valor: ',v[i].valor);
+    end;
+end;
+
+Procedure insercion ( var v: vector; dimL: Integer );
+var 
+    i, j, p: Integer; 
+    item : Integer;	
+    actual:oficina;
+begin
+    for i:=2 to dimL do 
+    begin 
+        actual:= v[i];
+        j:= i-1; 
+        while (j > 0) and (v[j].codigo > actual.codigo) do
+        begin
+            v[j+1]:= v[j];
+            j:=j-1;
+        end;  
+        v[j+1]:= actual; 
+    end;
+end;
+
+var
+    v:vector;
+    dimL:integer;
+begin
+    dimL:=0;
+    CargarOficinas(v,dimL);
+    ImprimirVector(v,dimL);
+    WriteL('__');
+    insercion(v,dimL);
+    ImprimirVector(v,dimL);
+end.
